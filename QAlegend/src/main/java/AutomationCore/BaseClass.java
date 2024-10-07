d125334f9fd4730ed7d1596d6f0618800dc5a34d
annmaria336 <164370127+annmaria336@users.noreply.github.com>
2024-10-07 11:41:04 -0400

    BaseClass.java

--- QAlegend/src/main/java/AutomationCore/BaseClass.java
+++ QAlegend/src/main/java/AutomationCore/BaseClass.java
@@ -36,7 +36,8 @@ public WebDriver browserInitilization(String browsername)
      {
 	System.out.println("invalid browser");
      }
-	driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS); //need to correct the code
+		//driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS); 
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 return driver;
 }	
 public String getScreenshotPath(String testcasename, WebDriver driver) throws IOException {
